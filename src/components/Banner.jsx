161cbfb79bdfb6c1410308f1baa21b2d6d3b08ed
Nafis <bhuiyannafis517@gmail.com>
2024-01-16 00:09:19 +0600

    l

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
         <div>
 <div className="hero max-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/j6s8Z77/360-F-335510693-HY7m-Lg3-ARd-Lcc-Ko-Xk3m66-NLDp-JRJh51p.jpg)'}}>
   <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-neutral-content text-center ">
+  <div className="hero-content text-neutral-content">
     <div className="max-w-md">
       <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
       <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
